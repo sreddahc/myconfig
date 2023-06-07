29b858fff289346255477d87186d4b55a4c7ba9c
Matt C <matthew-github@chadders.org>
2023-06-08 08:39:50 +1000

    Updated i3 and polybar configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+### Kill existing polybar sessions
 polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+### Manually launch bar on all monitors
+
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar secondary 2>&1 | tee -a /tmp/polybar1.log & disown
+
+# ### Try to launch on all monitors
+# screens=$(xrandr --listactivemonitors | cut -d " " -f 6)
+
+# for screen in $screens; do
+#     # echo $screen
+#     export MONITOR=$screen
+#     echo $MONITOR
+#     polybar main 2>&1 | tee -a /tmp/polybar1.log &
+#     disown
+#     sleep 1
+# done
