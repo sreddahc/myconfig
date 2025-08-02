4da6e61b3fc6d0ec3713c6b56433c8683f9a765e
chadders <matthew-github@chadders.org>
2025-08-03 03:15:59 +1000

    fix?

--- setup.sh
+++ setup.sh
@@ -9,8 +9,14 @@ fi
 if [ $(cat /etc/os-release | grep ^ID | cut -f 2 -d "=") = "fedora" ]; then
     sudo dnf install -y pipx python3-libdnf5
 fi
+if [ $(cat /etc/os-release | grep ^ID | cut -f 2 -d "=") = "rhel" ]; then
+    sudo dnf install -y pipx python3-libdnf5
+fi
+if [ $(cat /etc/os-release | grep ^ID | cut -f 2 -d "=") = "ubuntu" ]; then
+    sudo apt install -y git pipx
+fi
 pipx ensurepath
-source ~/.bashrc
+export PATH=$PATH:~/local/bin
 
 # Install Ansible
 pipx install --include-deps ansible
