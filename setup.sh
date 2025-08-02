3ceb319beea8bc7d15a50ae8940cd9a8a659db3f
chadders <matthew-github@chadders.org>
2025-08-03 02:36:29 +1000

    final config files

--- setup.sh
+++ setup.sh
@@ -5,26 +5,3 @@
 ######################
 ### WINDOW MANAGER ###
 ######################
-
-# Top bar font
-unzip ./fonts/NerdFontsSymbolsOnly.zip ./fonts/
-chmod 644 ./SymbolsNerdFont*
-mkdir -p ~/.local/share/fonts
-cp ./SymbolsNerdFont* ~/.local/share/fonts
-
-###############################
-### WINDOW MANAGER FEATURES ###
-###############################
-
-# Font
-sudo apt install -y fonts-powerline tmux
-
-########
-# BASH #
-########
-
-###############################
-# XMODMAP (KEYBOARD MAPPINGS) #
-###############################
-
-cp ./xmodmap/Xmodmap ~/.Xmodmap
