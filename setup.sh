6e003d3f03641496f7d972bd4ea3bb2ff4e22921
chadders <matthew-github@chadders.org>
2025-08-03 02:55:17 +1000

    updated install file

--- setup.sh
+++ setup.sh
@@ -2,13 +2,17 @@
 
 # Script to setup a system how I like to use it.
 
-# Install dependencies of Ansible
-# sudo apt install -y git pipx # If missing
-# sudo dnf install -y pipx python3-libdnf5
-# pipx ensurepath # If not in PATH
+# Install pipx
+if [ $(cat /etc/os-release | grep ^ID | cut -f 2 -d "=") = "debian" ]; then
+    sudo apt install -y git pipx
+fi
+if [ $(cat /etc/os-release | grep ^ID | cut -f 2 -d "=") = "fedora" ]; then
+    sudo dnf install -y pipx python3-libdnf5
+fi
+pipx ensurepath
 
 # Install Ansible
-# pipx install --include-deps ansible # If missing
+pipx install --include-deps ansible # If missing
 
 # Run Ansible
 cd ./ansible
