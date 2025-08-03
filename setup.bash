ed3cdf476a5a0bc68f4258e07471bc3083e9d0dc
chadders <matthew-github@chadders.org>
2025-08-03 12:32:03 +1000

    update system and cache 2.0

--- setup.bash
+++ setup.bash
@@ -4,15 +4,19 @@
 
 # Install pipx
 if [ $(cat /etc/os-release | grep ^ID | cut -f 2 -d "=") = "debian" ]; then
+    sudo apt upgrade -y
     sudo apt install -y git pipx
 fi
 if [ $(cat /etc/os-release | grep ^ID | cut -f 2 -d "=") = "fedora" ]; then
+    sudo dnf upgrade -y
     sudo dnf install -y pipx python3-libdnf5
 fi
 if [ $(cat /etc/os-release | grep ^ID | cut -f 2 -d "=") = "rhel" ]; then
+    sudo dnf upgrade -y
     sudo dnf install -y pipx python3-libdnf5
 fi
 if [ $(cat /etc/os-release | grep ^ID | cut -f 2 -d "=") = "ubuntu" ]; then
+    sudo apt upgrade -y
     sudo apt install -y git pipx
 fi
 pipx ensurepath
